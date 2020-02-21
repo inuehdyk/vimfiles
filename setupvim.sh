ef65ef23f42dc81cbfe76672ee49e29f2fbfaae5
inuehdyk <ob_admin@www.officebusters.com>
2020-02-21 11:45:13 +0900

    modify setup script

--- setupvim.sh
+++ setupvim.sh
@@ -4,6 +4,6 @@ mkdir -p ${VIMRC_DIR}/pack/my/start
 mkdir -p ${VIMRC_DIR}/pack/my/opt
 cd ${VIMRC_DIR}/pack/my/opt
 git clone https://github.com/vim-jp/vimdoc-ja.git
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo ${VIMRC_DIR}/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
